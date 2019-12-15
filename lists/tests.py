d66a5a3e62c615d53a1b48ead570e9f629a925f6
maoyf <maoyf@anchnet.com>
2019-12-15 22:43:17 +0800

    [Update]更新单元测试,使测试通过

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
-
+from django.http import HttpRequest
 from .views import home_page
 
 
@@ -11,3 +11,13 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/lists/home_page/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        """测试home_page页面能返回正常的页面"""
+        request = HttpRequest()  #创建了一个 HttpRequest 对象
+        response = home_page(request) #把这个 HttpRequest 对象传给 home_page 视图，得到响应。
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>',response.content)  # ➍
+        self.assertTrue(response.content.endswith(b'</html>')) #➎
+
+
