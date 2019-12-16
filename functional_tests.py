e1383c64bfe802a44fe3d483807d72b556dab632
maoyf <maoyf@anchnet.com>
2019-12-16 09:30:27 +0800

    [Refactor] Use template to test

--- functional_tests.py
+++ functional_tests.py
@@ -48,7 +48,7 @@ class NewVisitorTest(unittest.TestCase):
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
-            'Enter a  to-do item'
+            'Enter a to-do item'
         )
         # 她在一个文本框中输入了 “Buy peacock feathers”（购买孔雀羽毛）
         # allen的爱好是使用假蝇做饵钓鱼
