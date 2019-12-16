87206c66ee7ae30b58ac73e60f93aaeafa50f0f0
maoyf <maoyf@anchnet.com>
2019-12-16 15:39:03 +0800

    [Update] 更新视图函数

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
 # Create your views here.
 
 def home_page(request):
-    return render(request,'home.html')
+    if request.method == 'POST':
+        context = {'new_item_text':request.POST.get('item_text','')}
+        return render(request,'home.html',context)
+    return render(request,'home.html',)
