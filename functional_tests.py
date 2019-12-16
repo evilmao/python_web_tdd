0704dc9095b23c60f6276b99cf187b4ae817a5b3
maoyf <maoyf@anchnet.com>
2019-12-16 15:37:03 +0800

    [Test] 更新功能测试

--- functional_tests.py
+++ functional_tests.py
@@ -12,18 +12,6 @@ options = webdriver.ChromeOptions()
 options.binary_location = "D:\soft\Google\Chrome\Application\chrome.exe"
 
 
-# browser = webdriver.Chrome(chrome_options=options)
-
-# allen听说有一个很酷的在线待办事项应用
-# 她去看了这个应用的首页
-# browser.get('http://localhost:8000')
-
-# 她注意到网页的标题和头部都包含“To-Do”这个词
-# assert 'To-Do' in browser.title, "Browser title was " + browser.title
-
-# browser.quit()
-
-
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -43,7 +31,6 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-
         # 应用邀请她输入一个待办事项
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
@@ -60,18 +47,20 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:By peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
-
         # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
 
         # 页面再次更新，她的清单中显示了这两个待办事项
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         # 伊迪丝想知道这个网站是否会记住她的清单
         # 她看到网站为她生成了一个唯一的URL # 而且页面中有一些文字解说这个功能
 
