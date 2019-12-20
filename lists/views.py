179a5cacacbb523e8bb2b4aa2cb3975797c64b15
maoyf <maoyf@anchnet.com>
2019-12-20 10:39:42 +0800

    [Update] 添加新的视图用来展示所有的代办事项

--- lists/views.py
+++ lists/views.py
@@ -10,10 +10,17 @@ def home_page(request):
     if request.method == 'POST':  # 判断请求方式为'post'时
         new_item_text = request.POST['item_text']  # 获取前端提交的值赋值给变量
         Item.objects.create(text=new_item_text)  # 使用ORM, create方法保存数据
-        return redirect('/lists/')
+        return redirect('/lists/the-only-list-in-the-world/')
     # else:
     #     new_item_text = ''
     # context = {'new_item_text': new_item_text}
     # 当使用其他请求方式时,直接返回渲染模板
+    # 在 home_page 视图中其实也不用把全部待办事项都传入 home.html 模板
+    #return render(request, 'home.html',{'items':items})
+    # 可以简化成如下
+    return render(request, 'home.html')
+
+def view_list(request):
+    """list 视图函数"""
     items = Item.objects.all()
-    return render(request, 'home.html',{'items':items})
+    return render(request, 'list.html', {'items': items})
