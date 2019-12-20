aa49586cc866a10e2f9e354bb9449771b0d47e33
maoyf <maoyf@anchnet.com>
2019-12-20 10:37:54 +0800

    [Test] 新增单元测试

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,4 @@
 from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 
@@ -13,16 +12,16 @@ class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         """测试home_page的url是否使lists/home_page/"""
-        found = resolve('/lists/home_page/')
+        found = resolve('/lists/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_return_correct_html(self):
-        """测试home_page页面能返回正常的页面"""
-        request = HttpRequest()  # 创建了一个 HttpRequest 对象
-        response = home_page(request)  # 把这个 HttpRequest 对象传给 home_page 视图，得到响应。
-
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+    # def test_home_page_return_correct_html(self):
+    #     """测试home_page页面能返回正常的页面"""
+    #     request = HttpRequest()  # 创建了一个 HttpRequest 对象
+    #     response = home_page(request)  # 把这个 HttpRequest 对象传给 home_page 视图，得到响应。
+    #
+    #     expected_html = render_to_string('home.html')
+    #     self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
         """测试home_page视图可以保存一个post请求"""
@@ -33,21 +32,13 @@ class HomePageTest(TestCase):
 
         # 执行代码
         response = home_page(request)
-
+        # 编写断言
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()  # ➋
         self.assertEqual(new_item.text, 'A new list item')  # ➌
 
-        #编写断言
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-
-        self.assertEqual(response.content.decode(), expected_html)
         self.assertEqual(response.status_code,302)  # post请求成功后重定向,所以判断状态码是否是302
-        self.assertEqual(response['location'], '/lists/home_page/') # 获取重定向的url
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/') # 获取重定向的url
 
 
     def test_home_page_redirects_after_POST(self):
@@ -60,7 +51,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
         # 编写断言
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/home_page/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_home_page_only_saves_items_when_necessary(self):
         '''测试仅当需要时保存数据到模型'''
@@ -68,17 +59,6 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_display_all_list_items(self):
-        """测试首页展示所有条目"""
-        # 设置测试背景
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        # 执行代码
-        request = HttpRequest()
-        response = home_page(request)
-        # 编写断言
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 
@@ -102,3 +82,22 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest(TestCase):
+
+    def test_display_all_items(self):
+        """测试首页展示所有条目"""
+        # 设置测试背景
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        # 使用Django 测试客户端。
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        # 编写断言
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+
+    def test_users_list_template(self):
+        """检查是否使用了不同的模板"""
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response,'list.html')
