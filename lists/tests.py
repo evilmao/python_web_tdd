8a120303146d532dbf4002709725b913ee5176bb
maoyf <maoyf@anchnet.com>
2019-12-20 17:42:04 +0800

    [Test] 新增单元测试

--- lists/tests.py
+++ lists/tests.py
@@ -61,22 +61,36 @@ class ItemModelTest(TestCase):
 
 class ListViewTest(TestCase):
 
-    def test_display_all_items(self):
-        """测试首页展示所有条目"""
+    def test_displays_only_items_for_that_list(self):
+        """检查每一个代办事项都有唯一的URL"""
         # 设置测试背景
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
         # 使用Django 测试客户端。
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get('/lists/{}/'.format(correct_list.id))
         # 编写断言
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response,'other list item 1')
+        self.assertNotContains(response,'other list item 2')
 
     def test_users_list_template(self):
         """检查是否使用了不同的模板"""
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()                                # 插入一条新的List数据，返回queryset对象
+        response = self.client.get('/lists/{}/'.format(list_.id))    # 获取当前queryset的id属性
         self.assertTemplateUsed(response, 'list.html')
 
+    def test_passes_correct_list_to_template(self):
+        """检测"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.get('/lists/{}/'.format(correct_list.id))
+        self.assertEqual(response.context['list'],correct_list)
 
 class NewListTest(TestCase):
 
@@ -108,6 +122,37 @@ class NewListTest(TestCase):
             "/lists/new",
             data={'item_text': 'A new list item'}
         )
+        new_list = List.objects.first()
         # 编写断言
+
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response,'/lists/{}/'.format(new_list.id,))
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data = {'item_text': 'A new item for an existing list'}
+        )
+        # 断言
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_redirect_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data={'item_text': 'A new item for an existing list'}
+         )
+
+        self.assertRedirects(response, '/lists/{}/'.format(correct_list.id))
