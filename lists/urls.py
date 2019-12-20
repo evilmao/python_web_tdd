16455c531d941668ee2406cdf5fdbbc8722f97e6
maoyf <maoyf@anchnet.com>
2019-12-20 11:17:41 +0800

    [Update] 新增url

--- lists/urls.py
+++ lists/urls.py
@@ -11,4 +11,5 @@ app_name = "lists"
 urlpatterns = [
     path('', views.home_page, name='home-page'),
     path('the-only-list-in-the-world/',views.view_list, name='view_list'),
+    path('new',views.new_list, name='new_list'),
 ]
