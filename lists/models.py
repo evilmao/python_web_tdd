ca0cce6a1c3d025f61d4552488983e3038731d8f
maoyf <maoyf@anchnet.com>
2019-12-20 14:09:05 +0800

    [Update] 添加新的模型用来存储代办事项

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 
-class Item(models.Model):
+class List(models.Model):
+    pass
 
+
+class Item(models.Model):
     text = models.TextField(default="")
+    list = models.ForeignKey(List, default=None, on_delete=None,null=True)
