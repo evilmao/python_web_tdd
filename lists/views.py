887fc1225f5512926d1863c13244b6a007be0628
maoyf <maoyf@anchnet.com>
2019-12-20 17:43:04 +0800

    [Update] 添加新的视图函数

--- lists/views.py
+++ lists/views.py
@@ -10,14 +10,22 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request):
+def view_list(request,list_id):
     """list 视图函数"""
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+    list_ = List.objects.get(id=list_id)
+    # items = Item.objects.filter(list=list_)  # 可以使用反向查询取第
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
     """添加新代办事项视图"""
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'],list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect('/lists/{}/'.format(list_.id))
+
+
+def add_item(request,list_id):
+    """添加代办事项到代办列表中"""
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/{}/'.format(list_.id))
